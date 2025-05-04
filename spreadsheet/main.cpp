a5c8af171d8392a08fd3093d0ebccfd15ff8061b
Mikhail Sultanshin <1qa2wsx3edd@mail.ru>
2025-05-05 01:34:24 +0300

    Code Review #15

--- spreadsheet/main.cpp
+++ spreadsheet/main.cpp
@@ -1,3 +1,4 @@
+#include <limits>
 #include "common.h"
 #include "formula.h"
 #include "test_runner_p.h"
@@ -24,9 +25,6 @@ inline std::ostream& operator<<(std::ostream& output, const CellInterface::Value
 }
 
 namespace {
-std::string ToString(FormulaError::Category category) {
-    return std::string(FormulaError(category).ToString());
-}
 
 void TestPositionAndStringConversion() {
     auto testSingle = [](Position pos, std::string_view str) {
@@ -205,29 +203,29 @@ void TestErrorValue() {
                  CellInterface::Value(FormulaError::Category::Value));
 }
 
-void TestErrorDiv0() {
+void TestErrorArithmetic() {
     auto sheet = CreateSheet();
 
     constexpr double max = std::numeric_limits<double>::max();
 
     sheet->SetCell("A1"_pos, "=1/0");
     ASSERT_EQUAL(sheet->GetCell("A1"_pos)->GetValue(),
-                 CellInterface::Value(FormulaError::Category::Div0));
+                 CellInterface::Value(FormulaError::Category::Arithmetic));
 
     sheet->SetCell("A1"_pos, "=1e+200/1e-200");
     ASSERT_EQUAL(sheet->GetCell("A1"_pos)->GetValue(),
-                 CellInterface::Value(FormulaError::Category::Div0));
+                 CellInterface::Value(FormulaError::Category::Arithmetic));
 
     sheet->SetCell("A1"_pos, "=0/0");
     ASSERT_EQUAL(sheet->GetCell("A1"_pos)->GetValue(),
-                 CellInterface::Value(FormulaError::Category::Div0));
+                 CellInterface::Value(FormulaError::Category::Arithmetic));
 
     {
         std::ostringstream formula;
         formula << '=' << max << '+' << max;
         sheet->SetCell("A1"_pos, formula.str());
         ASSERT_EQUAL(sheet->GetCell("A1"_pos)->GetValue(),
-                     CellInterface::Value(FormulaError::Category::Div0));
+                     CellInterface::Value(FormulaError::Category::Arithmetic));
     }
 
     {
@@ -235,7 +233,7 @@ void TestErrorDiv0() {
         formula << '=' << -max << '-' << max;
         sheet->SetCell("A1"_pos, formula.str());
         ASSERT_EQUAL(sheet->GetCell("A1"_pos)->GetValue(),
-                     CellInterface::Value(FormulaError::Category::Div0));
+                     CellInterface::Value(FormulaError::Category::Arithmetic));
     }
 
     {
@@ -243,14 +241,14 @@ void TestErrorDiv0() {
         formula << '=' << max << '*' << max;
         sheet->SetCell("A1"_pos, formula.str());
         ASSERT_EQUAL(sheet->GetCell("A1"_pos)->GetValue(),
-                     CellInterface::Value(FormulaError::Category::Div0));
+                     CellInterface::Value(FormulaError::Category::Arithmetic));
     }
 }
 
 void TestEmptyCellTreatedAsZero() {
     auto sheet = CreateSheet();
     sheet->SetCell("A1"_pos, "=B2");
-    ASSERT_EQUAL(sheet->GetCell("A1"_pos)->GetValue(), CellInterface::Value(0));
+    ASSERT_EQUAL(sheet->GetCell("A1"_pos)->GetValue(), CellInterface::Value(0.0));
 }
 
 void TestFormulaInvalidPosition() {
@@ -363,11 +361,12 @@ int main() {
     RUN_TEST(tr, TestFormulaExpressionFormatting);
     RUN_TEST(tr, TestFormulaReferencedCells);
     RUN_TEST(tr, TestErrorValue);
-    RUN_TEST(tr, TestErrorDiv0);
+    RUN_TEST(tr, TestErrorArithmetic);
     RUN_TEST(tr, TestEmptyCellTreatedAsZero);
     RUN_TEST(tr, TestFormulaInvalidPosition);
     RUN_TEST(tr, TestPrint);
     RUN_TEST(tr, TestCellReferences);
     RUN_TEST(tr, TestFormulaIncorrect);
     RUN_TEST(tr, TestCellCircularReferences);
+    return 0;
 }
--- spreadsheet/sheet.h
+++ spreadsheet/sheet.h
@@ -1,1 +1,49 @@
-/* разместите здесь свой код */
+#pragma once
+
+#include "cell.h"
+#include "common.h"
+
+#include <functional>
+#include <unordered_map>
+
+namespace std {
+    template<>
+    struct hash<Position> {
+        size_t operator()(const Position& pos) const noexcept {
+            // Упаковываем 2 числа в 32 бита (14 бит на row, 14 бит на col)
+            // Оставшиеся 4 бита можно игнорировать
+            const uint32_t key = 
+                (static_cast<uint32_t>(pos.row) << 14) | 
+                static_cast<uint32_t>(pos.col);
+            
+            return hash<uint32_t>{}(key);
+        }
+    };
+}
+
+
+
+class Sheet : public SheetInterface {
+public:
+    //using Table = std::unordered_map<Position, std::unique_ptr<Cell>>;
+
+    ~Sheet();
+
+    void SetCell(Position pos, std::string text) override;
+
+    const CellInterface* GetCell(Position pos) const override;
+    CellInterface* GetCell(Position pos) override;
+
+    void ClearCell(Position pos) override;
+
+    Size GetPrintableSize() const override;
+
+    void PrintValues(std::ostream& output) const override;
+    void PrintTexts(std::ostream& output) const override;
+
+    const Cell* GetCellPtr(Position pos) const;
+    Cell* GetCellPtr(Position pos);
+
+private:
+    std::unordered_map<Position, std::unique_ptr<Cell>> cells_;
+};
